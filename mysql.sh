f6c0a301aeadcb988816914bcd5cd4e6d39ef734
NarendraReddy <narendra.nya@gmail.com>
2024-04-18 17:34:37 -0700

    installation

--- mysql.sh
+++ mysql.sh
@@ -2,7 +2,22 @@
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
-echo $LOGFILE
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+BL="\e[34m"
+N="\e[0m"
+
+VALIDATE()
+{
+    if [ $1 -ne 0 ];
+    then 
+        echo -e "$2 is ... $R FAILURE $N"
+    else 
+        echo -e "$2 is ... $G SUCCESS $N"    
+    fi
+}
 
 USERID=$(id -u)
 
@@ -12,4 +27,16 @@ then
     exit 1
 else 
     echo "Super User"     
-fi     
+fi  
+
+dnf install mysql-server -y &>>$LOGFILE 
+VALIDATE $? "mysql server installtion: "
+
+systemctl enable mysqld &>>$LOGFILE
+VALIDATE $? "enable mysqld server:  "
+
+systemctl start mysqld &>>$LOGFILE
+VALIDATE $? "start mysqld server:  "
+
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+VALIDATE $? "Set DB username and password: "
